b8d57c4cad8b0dba5e6302418cd16a37a26922d7
alexiordachescu <alex.iordachescu27@gmail.com>
2020-11-29 00:07:13 +0100

    set my-groups page layout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,6 +8,8 @@ import { selectGroups } from "../../store/group/selectors";
 import { selectUser } from "../../store/user/selectors";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -21,9 +23,22 @@ export default function Home() {
   const group = useSelector(selectGroups);
   const user = useSelector(selectUser);
 
+  // STYLING:
+
+  const useStyles = makeStyles({
+    typography: { fontSize: 25 },
+  });
+  const classes = useStyles();
+
   return (
     <div>
-      <h2>Browse groups</h2>
+      <Typography
+        variant="overline"
+        color="inherit"
+        className={classes.typography}
+      >
+        Browse groups
+      </Typography>
       <Grid container>
         <Grid item xs={2}>
           <h2>Filters</h2>
