6231b9bed8a3c693ec2182e010e19888bbc60fc0
alexiordachescu <alex.iordachescu27@gmail.com>
2020-11-29 15:50:42 +0100

    add mandatory fields for new users on signup page

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
+import Button from "@material-ui/core/Button";
 import { signUp } from "../../store/user/actions";
 import { selectToken } from "../../store/user/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
 import { Col } from "react-bootstrap";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
 export default function SignUp() {
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
   const dispatch = useDispatch();
   const token = useSelector(selectToken);
   const history = useHistory();
@@ -24,23 +30,63 @@ export default function SignUp() {
 
   function submitForm(event) {
     event.preventDefault();
-
-    dispatch(signUp(name, email, password));
+    console.log("firstname:", firstName);
+    console.log("lastName", lastName);
+    console.log("descripion", description);
+    console.log("image", imageUrl);
+    dispatch(
+      signUp(firstName, lastName, description, email, password, imageUrl)
+    );
 
     setEmail("");
     setPassword("");
-    setName("");
+    setFirstName("");
+    setLastName("");
+    setDescription("");
   }
+
+  const uploadImage = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "s5ct6hmo");
+
+    const res = await fetch(
+      "	https://api.cloudinary.com/v1_1/dmqbltypk/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+    const file = await res.json();
+    setImageUrl(file.secure_url);
+  };
+
+  const useStyles = makeStyles({
+    input: { display: "none" },
+    root: { maxWidth: 800 },
+  });
+  const classes = useStyles();
 
   return (
     <Container>
       <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
         <h1 className="mt-5 mb-5">Signup</h1>
         <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
+          <Form.Label>First Name</Form.Label>
           <Form.Control
-            value={name}
-            onChange={(event) => setName(event.target.value)}
+            value={firstName}
+            onChange={(event) => setFirstName(event.target.value)}
+            type="text"
+            placeholder="Enter name"
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            value={lastName}
+            onChange={(event) => setLastName(event.target.value)}
             type="text"
             placeholder="Enter name"
             required
@@ -59,7 +105,6 @@ export default function SignUp() {
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
-
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control
@@ -70,6 +115,36 @@ export default function SignUp() {
             required
           />
         </Form.Group>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Please write a short description of yourself</Form.Label>
+          <Form.Control
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            type="text"
+            placeholder="Enter name"
+            required
+          />
+        </Form.Group>
+        <Typography variant="overline">
+          Choose a representative photo of yourself
+        </Typography>
+        <input
+          type="file"
+          name="file"
+          className={classes.input}
+          onChange={uploadImage}
+          id="contained-button-file"
+        />{" "}
+        <label htmlFor="contained-button-file">
+          <Button
+            variant="contained"
+            color="primary"
+            component="span"
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload
+          </Button>
+        </label>
         <Form.Group className="mt-5">
           <Button variant="primary" type="submit" onClick={submitForm}>
             Sign up
--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -26,12 +26,22 @@ const tokenStillValid = (userWithoutToken) => ({
 
 export const logOut = () => ({ type: LOG_OUT });
 
-export const signUp = (name, email, password) => {
+export const signUp = (
+  firstName,
+  lastName,
+  description,
+  imageUrl,
+  email,
+  password
+) => {
   return async (dispatch, getState) => {
     dispatch(appLoading());
     try {
       const response = await axios.post(`${apiUrl}/signup`, {
-        name,
+        firstName,
+        lastName,
+        description,
+        imageUrl,
         email,
         password,
       });
